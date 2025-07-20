adec209ce6d0643cf4a5229b3a356844af2abe43
Chris McLeod <chris@chrismcleod.dev>
2025-07-20 17:30:16 +0100

    Refactor navigation module to fetch hobby data dynamically and update Paintslam URL to reflect the latest year

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,38 +1,46 @@
-module.exports = {
-  top: [
-    {
-      text: 'Blog',
-      url: '/blog/'
-    },
-    {
-      text: 'Bookmarks',
-      url: '/bookmarks/'
-    },
-    {
-      text: 'Paintslam',
-      url: '/paintslam/2025/'
-    },
-    {
-      text: 'Photos',
-      url: 'https://chrismcleod.photos/'
-    },
-    {
-      text: 'Follow',
-      url: '/follow/'
-    },
-    {
-      text: 'About',
-      url: '/about/'
-    }
-  ],
-  bottom: [
-    {
-      text: 'Privacy',
-      url: '/privacy/'
-    },
-    {
-      text: 'Roadmap',
-      url: '/roadmap/'
-    }
-  ]
+const getHobbyData = require('./hobby.js');
+
+module.exports = async function() {
+  const hobby = await getHobbyData();
+  const years = Object.keys(hobby).map(Number);
+  const latestYear = years.length ? Math.max(...years) : new Date().getFullYear();
+
+  return {
+    top: [
+      {
+        text: 'Blog',
+        url: '/blog/'
+      },
+      {
+        text: 'Bookmarks',
+        url: '/bookmarks/0/'
+      },
+      {
+        text: 'Paintslam',
+        url: `/paintslam/${latestYear}/`
+      },
+      {
+        text: 'Photos',
+        url: 'https://chrismcleod.photos/'
+      },
+      {
+        text: 'Follow',
+        url: '/follow/'
+      },
+      {
+        text: 'About',
+        url: '/about/'
+      }
+    ],
+    bottom: [
+      {
+        text: 'Privacy',
+        url: '/privacy/'
+      },
+      {
+        text: 'Roadmap',
+        url: '/roadmap/'
+      }
+    ]
+  };
 };
